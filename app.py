ba7e328fc16f2def56b1835675d8737e595b21dd
nomadan68912 <118900725+nomadan68912@users.noreply.github.com>
2023-04-19 17:54:02 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,14 @@
+import os
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    html = '''
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <title>美味しいデザート</title>
-        <meta charset="utf-8"/>
-    </head>
-    <body>
-        <h1>デザート一覧</h1>
-        <ul>
-            <li>アイス</li>
-            <li>ケーキ</li>
-            <li>パフェ</li>
-    </body>
-</html>'''
-    return html
+@app.route("/")
+def main():
+    return "Welcome!"
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='localhost')
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
