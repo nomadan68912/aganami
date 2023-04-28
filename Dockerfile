7e8e6c5ab01331dbb8184aebf12214148014ed07
nomadan68912 <118900725+nomadan68912@users.noreply.github.com>
2023-04-28 14:22:40 +0900

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
-FROM ubuntu:16.04
+FROM python:alpine
 
-ENV COLOR "red"
-RUN apt-get update -y && \
-    apt-get install -y python-pip python-dev
-
-# We copy just the requirements.txt first to leverage Docker cache
 COPY ./requirements.txt /app/requirements.txt
 
 WORKDIR /app
--- app.py
+++ app.py
@@ -2,12 +2,6 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    color = os.environ.get('COLOR', "green") 
-    message = "Welcome !!! This is "+color+" environment" 
-    return message
-
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
