0be0bcc0d757285c06f86ab8605edef33364df68
nomadan68912 <118900725+nomadan68912@users.noreply.github.com>
2023-05-19 19:29:39 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ def index():
             <p>３．OpenShift環境にクラウド監視製品（Instanaエージェントのコンテナ）導入</p>
             <p>４．Instanaのインフラストラクチャー画面で監視対象にしたOpenShiftと作成したPythonコンテナが監視対象となっていることの確認</p>
             
-            <p><a href="https://www.nicsoft.co.jp/">ソフトのホームぺ～ジ～</a></p>
     </body>
 </html>'''
     return html
