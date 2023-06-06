d8146c86830670683824207e82a6f94c8a23d5c2
nomadan68912 <118900725+nomadan68912@users.noreply.github.com>
2023-06-06 13:12:42 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -10,6 +10,15 @@ def index():
     <head>
         <title>第1回SE塾「コンテナの知識について（基本編）」</title>
         <meta charset="utf-8"/>
+        <script>
+            (function(s,t,a,n){s[t]||(s[t]=a,n=s[a]=function(){n.q.push(arguments)},
+            n.q=[],n.v=2,n.l=1*new Date)})(window,"InstanaEumObject","ineum");
+
+            ineum('reportingUrl', 'https://eum-orange-saas.instana.io');
+            ineum('key', 'GMyajlc7Ss-2lLTxoFqslg');
+            ineum('trackSessions');
+            </script>
+            <script defer crossorigin="anonymous" src="https://eum.instana.io/eum.min.js"></script>
     </head>
     <body>
         <h2>◆GitHubのサンプルコードをOpenShiftでコンテナ化してクラウド監視製品で監視対象にしてみる！</h2>
@@ -19,6 +28,7 @@ def index():
             <p>３．OpenShift環境にクラウド監視製品（Instanaエージェントのコンテナ）導入</p>
             <p>４．Instanaのインフラストラクチャー画面で監視対象にしたOpenShiftと作成したPythonコンテナが監視対象となっていることの確認</p>
             
+            <p><a href="https://www.nicsoft.co.jp/">ソフトのホームぺ～ジ～</a></p>
     </body>
 </html>'''
     return html
