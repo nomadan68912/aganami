7b2d01c5dc3d3f93f517330dd8902a794cd78d6e
nomadan68912 <118900725+nomadan68912@users.noreply.github.com>
2023-06-21 12:58:59 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -8,27 +8,27 @@ def index():
 <!DOCTYPE html>
 <html lang="ja">
     <head>
-        <title>第1回SE塾「コンテナの知識について（基本編）」</title>
+        <title>第1回 Instana Observability Meetup</title>
         <meta charset="utf-8"/>
         <script>
-            (function(s,t,a,n){s[t]||(s[t]=a,n=s[a]=function(){n.q.push(arguments)},
-            n.q=[],n.v=2,n.l=1*new Date)})(window,"InstanaEumObject","ineum");
+           (function(s,t,a,n){s[t]||(s[t]=a,n=s[a]=function(){n.q.push(arguments)},
+           n.q=[],n.v=2,n.l=1*new Date)})(window,"InstanaEumObject","ineum");
 
-            ineum('reportingUrl', 'https://eum-orange-saas.instana.io');
-            ineum('key', 'GMyajlc7Ss-2lLTxoFqslg');
-            ineum('trackSessions');
-            </script>
-            <script defer crossorigin="anonymous" src="https://eum.instana.io/eum.min.js"></script>
+           ineum('reportingUrl', 'https://instana.automate-your.work:446/eum/');
+           ineum('key', 'MqMhdpz0TluBtZwlllJeXg');
+           ineum('trackSessions');
+        </script>
+        <script defer crossorigin="anonymous" src="https://instana.automate-your.work:446/eum/eum.min.js"></script>
     </head>
     <body>
-        <h2>◆GitHubのサンプルコードをOpenShiftでコンテナ化してクラウド監視製品で監視対象にしてみる！</h2>
+        <h2>◆◆◆◆し買う第1回 Instana Observability Meetup◆◆◆◆◆</h2>
         <ul>
             <p>１．コンテナ概要の説明</p>
             <p>２．OpenShift環境にPythonコンテナを作成</p>
             <p>３．OpenShift環境にクラウド監視製品（Instanaエージェントのコンテナ）導入</p>
             <p>４．Instanaのインフラストラクチャー画面で監視対象にしたOpenShiftと作成したPythonコンテナが監視対象となっていることの確認</p>
             
-            <p><a href="https://www.nicsoft.co.jp/">ソフトのホームぺ～ジ～</a></p>
+            <p><a href="https://www.nicsoft.co.jp/">リンク</a></p>
     </body>
 </html>'''
     return html
