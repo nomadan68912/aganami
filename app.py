56ec753986b5bfe086c0142243cda7fce3c611c6
nomadan68912 <118900725+nomadan68912@users.noreply.github.com>
2023-06-22 15:10:37 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ def index():
         <script defer crossorigin="anonymous" src="https://instana.automate-your.work:446/eum/eum.min.js"></script>
     </head>
     <body>
-        <h2>◆◆◆◆し買う第1回 Instana Observability Meetup◆◆◆◆◆</h2>
+        <h2>◆◆◆◆第1回 Instana Observability Meetup◆◆◆◆◆</h2>
         <ul>
             <p>１．コンテナ概要の説明</p>
             <p>２．OpenShift環境にPythonコンテナを作成</p>
-            <p>３．OpenShift環境にクラウド監視製品（Instanaエージェントのコンテナ）導入</p>
+            <p>３．OpenShift環境にInstanaエージェント導入</p>
             <p>４．Instanaのインフラストラクチャー画面で監視対象にしたOpenShiftと作成したPythonコンテナが監視対象となっていることの確認</p>
             
             <p><a href="https://www.nicsoft.co.jp/">リンク</a></p>
